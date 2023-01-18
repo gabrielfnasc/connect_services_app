5e0a722d517fc9b4da14fc9a95a32b94fc523533
GabrielfNascimento <gf31956@gmail.com>
2023-01-18 10:00:40 -0300

    inital architecture

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:connect_services_app/main.dart';
+import 'package:connect_services_app/src/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
